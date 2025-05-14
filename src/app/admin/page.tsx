774f316d1f47101252ab09f026aa5a288aec3a90
Eneas Baroni <eneasbaroni@gmail.com>
2025-05-14 18:22:27 -0300

    admin init

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+    title: "Cecilia Torres - Panel Administrador",
+    description: "Cecilia Torres-Tanatóloga",
+    //icon
+    icons: {
+        icon: "./images/icon.svg",
+        shortcut: "./images/icon.svg",
+        apple: "./images/icon.svg",
+    },
+};
+
+const Admin = () => {
+    return <div>page</div>;
+};
+export default Admin;
