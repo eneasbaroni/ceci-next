6fdd081a48ddef0b2a2d50e7b348e882b1944c0d
Eneas Baroni <eneasbaroni@gmail.com>
2026-03-23 12:57:18 -0300

    fix npm i conflicts

--- src/app/(withLayout)/purchase/[id]/page.tsx
+++ src/app/(withLayout)/purchase/[id]/page.tsx
@@ -1,6 +1,6 @@
-import books from './books'
+import books from "./books";
 import { Metadata } from "next";
-import UserForm from '../components/UserForm'
+import UserForm from "../components/UserForm";
 
 export const metadata: Metadata = {
     title: "Cecilia Torres - Adquirir libro",
@@ -13,23 +13,26 @@ export const metadata: Metadata = {
     },
 };
 
+const Purchase = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
 
-const Purchase = ({ params: { id } }: { params: { id: string } }) => {
+    const book = books.find((book) => book.id === id);
 
-  const book = books.find(book => book.id === id)
+    return (
+        <main id="purchase" className="">
+            <div className="loaderContainer displayNone" id="loader">
+                <div className="loader"></div>
+            </div>
+            <h1>Adquirir &quot;{book?.title}&quot;</h1>
+            <p>
+                Para adquirir el libro, completa el siguiente formulario con tus
+                datos para habilitar la pasarela de pago. <br />
+                El costo de envío no está incluido, el mismo se abona al momento
+                de coordinar la entrega.
+            </p>
+            <UserForm book={book!} />
+        </main>
+    );
+};
 
-  return (
-
-    <main id="purchase" className="">
-      <div className="loaderContainer displayNone" id="loader">
-        <div className="loader"></div>
-      </div>
-      <h1>Adquirir &quot;{book?.title}&quot;</h1> 
-      <p>Para adquirir el libro, completa el siguiente formulario con tus datos para habilitar la pasarela de pago. <br/>El costo de envío no está incluido, el mismo se abona al momento de coordinar la entrega.</p>
-      <UserForm book={book!}/>   
-    </main>
-
-  )
-}
-
-export default Purchase
+export default Purchase;
