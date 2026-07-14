04299d570ff60352a5693f991966155d3e815fe6
Eneas Baroni <eneasbaroni@gmail.com>
2026-07-14 17:35:38 -0300

    force dinamic

--- src/app/(withLayout)/workshops/page.tsx
+++ src/app/(withLayout)/workshops/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { Metadata } from 'next'
 import { connectMongoDB } from '../../../lib/mongodb'
 import Workshop from '../../api/DAO/models/workshop.model'
